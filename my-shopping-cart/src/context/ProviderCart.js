7f8a09cfbda866f511fea8cb78183fc0789861a1
Leandro O. Bastos <obastosleandro@gmail.com>
2022-05-30 15:56:08 -0300

    Loading spinner 3/8

--- my-shopping-cart/src/context/ProviderCart.js
+++ my-shopping-cart/src/context/ProviderCart.js
@@ -3,8 +3,14 @@ import MyContext from './MyContext';
 
 export default function ProviderCart({ children }) {
   const [cartProducts, setCartProducts] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
   return (
-    <MyContext.Provider value={{ cartProducts, setCartProducts }}>
+    <MyContext.Provider value={{
+      cartProducts,
+      setCartProducts,
+      isLoading,
+      setIsLoading
+    }}>
       {children}
     </MyContext.Provider>
   )
